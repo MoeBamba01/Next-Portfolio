5eae5ac93b4508accb99ea47929c3c33eec1198e
moebamba01 <djigal001@gmail.com>
2024-01-14 01:35:42 +0000

    Final Touch

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -32,9 +32,9 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-white mb-3 text-3xl sm:text-4xl lg:text-5xl lg:leading-normal font-extrabold">
+          <h1 className="text-white mb-3 text-3xl sm:text-4xl lg:text-6xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hi, I'm Moe Bamba, a
+              Hello, I'ts Moe Bamba ,I'm a
               <br />
             </span>
             <TypeAnimation
