159012958867b06cfd8301117771be6fb1d2e81c
moebamba01 <djigal001@gmail.com>
2024-01-18 16:46:09 +0000

    Final

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import Image from "next/image";
+import Logo from "../../../public/images/LOGO.svg"
 
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span>Mouhamadou Bamba Djigal</span>
+        <Image src={Logo} alt="Logo" width={100} height={100}/>
         <p className="text-slate-600">All rights reserved.</p>
       </div>
     </footer>
   );
 };
 
-export default Footer;
+export default Footer;  
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,6 +4,8 @@ import React, { useState } from "react";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
+import Logo from "../../../public/images/LOGO.svg"
+import Image from "next/image";
 
 const navLinks = [
   {
@@ -30,7 +32,7 @@ const Navbar = () => {
           href={"/"}
           className="text-2xl md:text-5xl text-white font-semibold "
         >
-          M<span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">B</span>
+          <Image src={Logo} alt="Logo" width={105} height={105}/>
         </Link>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
