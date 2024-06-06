dd3a6703c46c5e14c7bf015e85d3d8d1a3cdea37
moebamba01 <djigal001@gmail.com>
2024-06-06 11:25:10 +0000

    Adding Typescript

---
+++ src/app/components/CertifsSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CertifsSection = () => {
+    return (
+        <div>
+            {/* Your component code here */}
+        </div>
+    );
+};
+
+export default CertifsSection;
--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -55,7 +55,7 @@ const EmailSection = () => {
           I&apos;m currently looking for new opportunities, my inbox is always
           open. Whether you have a question or just want to say hi, I&apos;ll
           try my best to get back to you!
-        </p>
+        </p >
         <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/MoeBamba01" target="_blank" rel="noopener noreferrer">
             <Image src={GithubIcon} alt="Github Icon" />
